c61a2c3a75e39ce30b291837809465ee1828adf4
gudodo16 <31950075+gudodo16@users.noreply.github.com>
2021-03-22 14:57:47 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -20,9 +20,7 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			$dood =$event['text'];
-			if(){
-
-			}
+			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
